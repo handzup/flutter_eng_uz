49e7137027e2827e3b26f980673f39e7c804af77
DjuraevShakhzod <djuraevshakhzod@gmail.com>
2021-08-27 15:14:56 +0500

    fix: fix sh filne naming

---
+++ .github/scripts/decrypt_android_keys.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$MYKEY" \
+--output android/key.zip android/key.zip.gpg && cd android && jar xvf android_keys.zip && cd -
